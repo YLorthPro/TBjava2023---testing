7c21ba93b0db7a8e8210e3b7ddabefcf794f7590
YLorthPro <yann.lorthioir@bstorm.be>
2023-12-21 12:04:35 +0100

    Correction first tests

--- src/main/java/be/bstorm/formation/demotesting/demoSimple/DemoTestService.java
+++ src/main/java/be/bstorm/formation/demotesting/demoSimple/DemoTestService.java
@@ -2,7 +2,6 @@ package be.bstorm.formation.demotesting.demoSimple;
 
 import org.springframework.stereotype.Service;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
 
--- src/test/java/be/bstorm/formation/demotesting/demoSimple/DemoTestServiceTest.java
+++ src/test/java/be/bstorm/formation/demotesting/demoSimple/DemoTestServiceTest.java
@@ -1,9 +1,12 @@
 package be.bstorm.formation.demotesting.demoSimple;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class DemoTestServiceTest {
 
@@ -51,4 +54,72 @@ public class DemoTestServiceTest {
         // Act & Assert
         assertThrows(ArithmeticException.class, ()->demoTestService.diviser(a,b));
     }
+
+    @Test
+    public void test_addition(){
+        //Arange
+        int a = 10;
+        int b = 5;
+
+        //Act
+        int resultat = demoTestService.addition(a,b);
+
+        //Assert
+        assertEquals(15,resultat);
+    }
+
+    @Test
+    public void test_split_ok(){
+        //Arange
+        String phrase = "Hello World";
+
+        //Act
+        List<String> resultat = demoTestService.split(phrase);
+
+        //Assert
+        assertEquals(Arrays.asList("Hello","World"), resultat);
+    }
+
+    @Test
+    public void test_split_notok(){
+        //Arange
+        String phrase = null;
+
+        //Act & Assert
+        assertThrows(NullPointerException.class,()->demoTestService.split(phrase));
+    }
+
+    @Test
+    public void test_lowercase_ok(){
+        //Arange
+        String phrase = "test";
+
+        //Act
+        boolean resultat = demoTestService.isLowercase(phrase);
+
+        //Assert
+        assertTrue(resultat);
+    }
+
+    @Test
+    public void test_lowercase_ko(){
+        //Arange
+        String phrase = "Test";
+
+        //Act
+        boolean resultat = demoTestService.isLowercase(phrase);
+
+        //Assert
+        assertFalse(resultat);
+    }
+
+    @Test
+    public void test_lowercase_quand_null(){
+        //Arange
+        String phrase = null;
+
+        //Act & Assert
+        assertThrows(NullPointerException.class, ()->demoTestService.isLowercase(phrase));
+    }
+
 }
